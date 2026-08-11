be35bd09df5703391c37647dfeb2c5a1f63de99f
Invxrt <Ryliejay2016@gmail.com>
2026-08-11 15:03:53 +0100

    test

--- kubejs/server_scripts/mods/pneumaticcraft.js
+++
@@ -1,8 +0,0 @@
-ServerEvents.recipes((event) => {
-  event.remove({ output: "pneumaticcraft:reinforced_stone" });
-  event.shaped(
-    Item.of("pneumaticcraft:reinforced_stone", 8),
-    ["SSS", "SAS", "SSS"],
-    { S: "ad_astra:moon_stone", A: "pneumaticcraft:ingot_iron_compressed" },
-  );
-});
